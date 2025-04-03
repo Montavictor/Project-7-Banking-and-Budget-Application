17565dc484c829caae70a2609e11ef0c8c0d289f
Victor Monta <montavictor1@gmail.com>
2025-04-03 19:14:48 +0800

    CreateUser.jsx, trying user.json as database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateUser from './components/CreateUser/CreateUser';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <CreateUser />
     </div>
   );
 }

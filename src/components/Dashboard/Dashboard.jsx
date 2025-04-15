8ad3c76766bc18a32b83add8b7bc9948a3a62489
Sam Fernandez <fernandez.sjackson@gmail.com>
2025-04-15 15:57:52 +0800

    added basics of dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,67 @@
 import {useState} from 'react';
-import Box from '@mui/material/Box';
+import {
+    Box,
+    Paper,
+    Typography
+} from '@mui/material';
 
 
 
 export default function Dashboard () {
-    const [userList, setUserList] = useState(() => {
-        return JSON.parse(localStorage.getItem("bankUsers") || []);
-    })
-
+    const userList = JSON.parse(localStorage.getItem("bankUsers") || []);
+    
+    const findTransactions = userList.flatMap(user =>
+        user.transactions.map((t) => ({
+            ...t,
+            user: user.name,
+        }))
+    )
     return (
-        <Box></Box>
+        <Paper
+      elevation={8}
+      sx={{
+        maxWidth: "95%",
+        height: 310,
+        mx: "auto",
+        mt: 4,
+        display: "flex",
+        flexDirection: "column",
+      }}
+        >
+            <Paper
+                sx={{
+                p: 1,
+                display: "flex",
+                height: 40,
+                flexDirection: "column",
+                alignItems: "flex-start",
+                marginBottom: 3,
+                borderBottomRightRadius: 0,
+                borderBottomLeftRadius: 0,
+                background: "#c62828",
+                }}
+            > 
+            <Typography variant="outline" color="#ffff" sx={{ fontWeight: 700 }}>
+                Dashboard
+            </Typography>
+            </Paper>
+
+            <Box>
+                <Typography variant="h3" color="primary">
+                    {findTransactions.length}
+                </Typography>
+                <Typography variant="overline" gutterBottom>
+                    Total Transactions
+                </Typography>
+            </Box>
+            <Box>
+                <Typography variant="h3" color="primary">
+                    {userList.length}
+                </Typography>
+                <Typography variant="overline" gutterBottom>
+                    Total Users
+                </Typography>
+            </Box>
+        </Paper>
     )
 }
